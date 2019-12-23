83f48c68b47d5bb4d9e20720972c3392c4d1c315
Tony Germaneri <Tony.Germaneri@gmail.com>
2019-12-22 17:34:30 -0800

    removed extra log line

--- index.ts
+++ index.ts
@@ -168,7 +168,6 @@ class ServerlessCloudfrontDistributionCertificate {
       const getZones = (marker: string) => {
         // prevent Throttling: Rate exceeded
         setTimeout(() => {
-          this.serverless.cli.log(`Getting zone page:marker ` + marker);
           this.route53.listHostedZones({
             Marker: marker === "" ? undefined : marker,
             MaxItems: "100",
